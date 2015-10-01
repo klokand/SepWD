59dc5c0a3b747d71bd390d6a5261b9aede9ea476
klokand <xjz520223@gmail.com>
2015-10-01 10:49:25 +1000

    finalized blog page

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,93 +3,62 @@
 <ul id="box" class="col-md-8 col-md-offset-1">
   <li>
     <div class="lazy dh-container img-repsonsive" dataimg="/img/test1.jpg">
-	<div class="dh-overlay">Image Caption Here1</div>
+	<div class="dh-overlay"><h1>Chinese Herbal Medicine</h1><p>Chinese herbal medicine is an integral part of Traditional Chinese Medicine. It is one of the oldest continuously practiced medical systems in the world with a written history dating over 2,500 years.</p><a href="{{url('/')}}/blog" class="btn btn-primary">Find Out More</a></div>
     </div>
   </li>
   <li>
-    <div class="lazy dh-container"  dataimg="https://unsplash.it/375/225?image=763" />
-	<div class="dh-overlay">testsdfsdfwer<p><a href="google.com" class="btn btn-primary">Find Out More</a></p></div>
+    <div class="lazy dh-container img-repsonsive" dataimg="/img/test2.jpg">
+	<div class="dh-overlay"><h1>Translating the yin and yang of TCM</h1><p>Traditional Chinese medicine, involving concepts of yin and yang, balance and harmony, has been keeping people healthy for more than 2,000 years and practitioners want to share traditional healing with more people around the world. </p><a href="{{url('/')}}/blog" class="btn btn-primary">Find Out More</a></div>
     </div>
   </li>
   <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=761" />
+    <div class="lazy dh-container img-repsonsive" dataimg="/img/test3.jpg">
+	<div class="dh-overlay"><h1>TCM Treasures - Cong bai</h1><p>Cong bai, the white part between a spring onion's green leaves and its root, is a popular food and traditional Chinese medicine, used to treat ailments such as colds, dysentery, acute mastitis, urinary problems, and headaches and stomachache caused by chills.</p><a href="{{url('/')}}/blog" class="btn btn-primary">Find Out More</a></div>
+    </div>
+  </li>
+    <li>
+    <div class="lazy dh-container img-repsonsive" dataimg="/img/test4.jpg">
+	<div class="dh-overlay"><h1>Medicinal Baths Tone Up the Yao Ethnic Group</h1><p>Members of the Yao ethnic group in Jiang County, southwest China's Guizhou Province, usually bathe in medicinal water, an ancient and magical way to stay healthy.</p><a href="{{url('/')}}/blog" class="btn btn-primary">Find Out More</a></div>
     </div>
   </li>
   <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=760" />
+    <div class="lazy dh-container img-repsonsive" dataimg="/img/test1.jpg">
+	<div class="dh-overlay"><h1>Chinese Herbal Medicine</h1><p>Chinese herbal medicine is an integral part of Traditional Chinese Medicine. It is one of the oldest continuously practiced medical systems in the world with a written history dating over 2,500 years.</p><a href="{{url('/')}}/blog" class="btn btn-primary">Find Out More</a></div>
     </div>
   </li>
   <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=759" />
+    <div class="lazy dh-container img-repsonsive" dataimg="/img/test2.jpg">
+	<div class="dh-overlay"><h1>Translating the yin and yang of TCM</h1><p>Traditional Chinese medicine, involving concepts of yin and yang, balance and harmony, has been keeping people healthy for more than 2,000 years and practitioners want to share traditional healing with more people around the world. </p><a href="{{url('/')}}/blog" class="btn btn-primary">Find Out More</a></div>
     </div>
   </li>
   <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=758" />
+    <div class="lazy dh-container img-repsonsive" dataimg="/img/test3.jpg">
+	<div class="dh-overlay"><h1>TCM Treasures - Cong bai</h1><p>Cong bai, the white part between a spring onion's green leaves and its root, is a popular food and traditional Chinese medicine, used to treat ailments such as colds, dysentery, acute mastitis, urinary problems, and headaches and stomachache caused by chills.</p><a href="{{url('/')}}/blog" class="btn btn-primary">Find Out More</a></div>
     </div>
   </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=757" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=756" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=755" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=580" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=570" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=560" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=540" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=530" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=520" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=510" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=550" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=490" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=480" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=470" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=460" />
-    </div>
-  </li>
-  <li>
-    <div class="lazy"  dataimg="https://unsplash.it/375/225?image=450" />
+    <li>
+    <div class="lazy dh-container img-repsonsive" dataimg="/img/test4.jpg">
+	<div class="dh-overlay"><h1>Medicinal Baths Tone Up the Yao Ethnic Group</h1><p>Members of the Yao ethnic group in Jiang County, southwest China's Guizhou Province, usually bathe in medicinal water, an ancient and magical way to stay healthy.</p><a href="{{url('/')}}/blog" class="btn btn-primary">Find Out More</a></div>
     </div>
   </li>
 </ul>
+<nav class="col-md-8 col-md-offset-1">
+  <ul class="pagination pagination-lg">
+    <li>
+      <a href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li><a href="#">1</a></li>
+    <li><a href="#">2</a></li>
+    <li><a href="#">3</a></li>
+    <li><a href="#">4</a></li>
+    <li><a href="#">5</a></li>
+    <li>
+      <a href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
 @endsection
