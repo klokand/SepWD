8b3af3dafb46ff0600f928b2c8b5124d02054692
klokand <xjz520223@gmail.com>
2015-10-04 21:12:15 +1100

    768 max style

--- resources/views/fees.blade.php
+++ resources/views/fees.blade.php
@@ -1,4 +1,47 @@
 @extends('app')
 @section('content')
+<div class="passage col-md-10 col-md-offset-1">
+<img class="img-responsive line" src="/img/line.png">
+<div class="col-md-8">
+<h1>Fees and Rebates</h1>
+<h3>TCM consultation and acupuncture treatment</h3>
+<p>Initial consultation with acupuncture treatment   $110<p>
+<p>Standard consultation with acupuncture treatment                     $70</p>
+<p>Second consultation with acupuncture within same week
 
+ (Starts on Monday and ends on Sunday)                            $60</p>
+ <p>Acupuncture treatments for embryo transfers (within 24h)        $110(2 sessions)</p>
+ <p>Initial consultation                                                 $60</p>
+ <p>Standard consultation                                              $30</p>
+ <p>Additional moxibustion/cupping/scrapping                            $10each</p>
+ 
+<h3>Couples</h3>
+<p>Initial consultation and treatment for both partners                     $190</p>
+<p>Standard consultation+treatment for both in same week                $130</p>
+<h3>Concession or health card</h3>
+<p>Initial consultation with acupuncture treatment                        $95</p>
+<p>Standard consultation with acupuncture treatment                     $65</p>
+<h3>Chinese Herbal Medicine</h3>
+<p>Herbs are charged separately should they be required. Our Chinese Herbal
+
+includes Granulated powder and Patent Herbal Pills.</p>
+<p>Chinese Herbal Medicine Formula                              $55-$95/2 weeks</p>
+<h3>Remedial Massage</h3>
+<p>30 Minutes                                 $55</p>
+<p>45 minutes                                 $70</p>
+<p>60 minutes                                 $85</p>
+<h3>Private Health Fund Rebates</h3>
+<ul><li>For acupuncture and remedial massage treatment, all Health Fund Rebates
+
+are Available depending on your level of cover. Please check with your health 
+
+insurance.</li>
+<li>For Chinese herbal medicine, some health fund rebate are available. Please
+check with your health insurance.</li></ul>
+<h3>Cancellations</h3>
+<p>If you cannot attend your appointment or if you need to reschedule, we require 24hrs 
+
+notice, otherwise a cancellation fee $30 will be charged.</p>
+</div>
+</div>
 @endsection
