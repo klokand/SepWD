b8faaa7fadb7ddbf6dc07a2f92c81cfff64c3e9d
klokand <xjz520223@gmail.com>
2015-10-20 01:38:45 +1100

    update word of mail

--- resources/views/emails/booked.blade.php
+++ resources/views/emails/booked.blade.php
@@ -1,7 +1,7 @@
 <html>
 
 @if(isset($phone))
-   <head> New enquery from {{$name}}</head>
+   <head> New enquiry from {{$name}}</head>
 	<body>
 	<p>Name:{{$name}}</p>
 	<p>Email:{{$email}}</p>
@@ -9,7 +9,7 @@
 	<p>Message:{{$content}}</p>
 </body>
 @else
-	<head>Your enquery has been sent out</head>
+	<head>Your enquiry has been Received</head>
 	<body>One of our team members will contact you shortly.</body>
 @endif
 </html>
